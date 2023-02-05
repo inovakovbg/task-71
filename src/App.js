10ba09fe8a530cce4e41432dc037fb995c1afe31
Ivan Novakov <inovakov.bg@gmail.com>
2023-02-05 17:49:23 +0200

    ADd
    add to Document

--- src/App.js
+++ src/App.js
@@ -1,41 +1,37 @@
-
 import "./App.css";
-import React, { useState, useEffect } from 'react';
-
-const Document = ({ title, content, handleScroll }) => {
-  return (
-    <div>
-      <h2 className="title">{title}</h2>
-      <div className="content" onScroll={handleScroll}>
-        {content}
-      </div>
-    </div>
-  );
-};
+import Document from "./Components/Document";
+import React, { useState, useEffect } from "react";
 
 function App() {
-  const [content, setContent] = useState('');
-  const [onButton, setButton] = useState(false);
+
+  const [title, Settitle] = useState([]);
+  const [content, Setcontent] = useState();
+
+
+
 
   useEffect(() => {
-    fetch('https://jaspervdj.be/lorem-markdownum/markdown.txt')
+    Settitle("Terms and Conditions");
+    fetch(
+      "https://jaspervdj.be/lorem-markdownum/markdown.txt"
+    )
       .then(response => response.text())
-      .then(text => {
-        setContent(text);
-      });
-  }, []);
+      .then(text => Setcontent(text))
+  }, [])
 
-  const handleScroll = event => {
-    if (event.target.scrollTop + event.target.clientHeight >= event.target.scrollHeight) {
-      setButton(true);
-    }
-  };
+
+
+
+
 
   return (
+
     <div>
-      <Document title="Terms and Conditions" content={content} handleScroll={handleScroll} />
-      <button disabled={!onButton}>I Agree</button>
+      <Document title={title} content={content} />
     </div>
+
+
+
   );
 }
 
